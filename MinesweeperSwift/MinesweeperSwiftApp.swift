95b7fbaa8a5c43d7f264e9a9c33b65d16440a89a
Liplum <Li_plum@outlook.com>
2022-11-22 15:25:19 +0800

    Initial Commit

---
+++ MinesweeperSwift/MinesweeperSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  MinesweeperSwiftApp.swift
+//  MinesweeperSwift
+//
+//  Created by Liplum on 11/22/22.
+//
+
+import SwiftUI
+
+@main
+struct MinesweeperSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
